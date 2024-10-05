73b781934fa9305c9cdc404025180c870cd10dd0
ROHITK-BHAGAT <rohitkumarbhagat@ternaengg.ac.in>
2024-10-05 13:03:26 +0530

    more project option

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var tablinks=document.getElementsByClassName("tab-links");
 var tabcontents=document.getElementsByClassName("tab-contents");
 
+
 function opentab(tabname){
     for(tablink of tablinks){
         tablink.classList.remove("active-link");
@@ -12,6 +13,17 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+let moreBtn=document.getElementById('moreBtn');
+let moreProject=document.getElementById('moreProject');
+let moreProject1=document.getElementById('moreProject1');
+
+function extraProject(){
+    moreProject.style.display="block";
+    moreProject1.style.display="block";
+}
+moreBtn.addEventListener('click',extraProject)
+
 
 // responsive nav bar
 var sidemenu=document.getElementById("sidemenu");
